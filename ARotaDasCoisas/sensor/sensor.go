b3181a3da97946efbafe85590b41ca9f2fb15941
enejota-njs <ns03052006@gmail.com>
2026-03-20 13:26:12 -0300

    test

--- ARotaDasCoisas/sensor/sensor.go
+++ ARotaDasCoisas/sensor/sensor.go
@@ -1,23 +1,37 @@
 package sensor
 
 import (
+	"encoding/json"
 	"math/rand"
+	"net"
 	"time"
 )
 
-func ValueGenerator(sensors map[string]float64) {
-	rand.Seed(time.Now().UnixNano())
+type Sensor struct {
+	Temperature float64 `json:"temperature"`
+	Luminosity  float64 `json:"luminosity"`
+	Humidity    float64 `json:"humidity"`
+}
+
+func main() {
+	conn, err := net.Dial("udp", "localhost:5050")
+	if err != nil {
+		panic(err)
+	}
+	defer conn.Close()
 
 	for {
-		generatedTemperature := rand.Float64()
-		generatedLuminosity := rand.Float64()
+		rand.Seed(time.Now().UnixNano())
 
-		temperature := generatedTemperature*30 + 10
-		luminosity := generatedLuminosity
+		data := Sensor{
+			Temperature: rand.Float64(),
+			Luminosity:  rand.Float64(),
+			Humidity:    rand.Float64(),
+		}
 
-		sensors["temperature"] = temperature
-		sensors["luminosity"] = luminosity
+		values, _ := json.Marshal(data)
 
+		conn.Write(values)
 		time.Sleep(1 * time.Second)
 	}
 }
--- ARotaDasCoisas/server/server.go
+++ ARotaDasCoisas/server/server.go
@@ -1,30 +1,59 @@
 package main
 
 import (
-	"ARotaDasCoisas/sensor"
 	"fmt"
 	"net"
 )
 
-func main() {
-	listener, err := net.Listen("tcp", ":5555")
-	if err != nil {
-		panic(err)
-	}
-	defer listener.Close()
+func handler(conn net.Conn) {
+	defer conn.Close()
 
-	fmt.Println("Servidor inicializado.")
 
-	sensors := make(map[string]float64)
-	go sensor.ValueGenerator(sensors)
+}
 
+func connectionSensor(listenerSensor net.Listen) {
 	for {
-		conn, err := listener.Accept()
+		connSensor, err := listenerSensor.Accept()
+		if err != nil {
+			continue
+		}
+
+		go handle
+	}
+}
+
+func connectionClient(listener *net.Listen) {
+	for {
+		connClient, err := listenerClient.Accept()
 		if err != nil {
 			continue
 		}
 
 		fmt.Println("Cliente conectado.")
-		go HandleConnection(conn, sensors)
+		go HandleConnection(connClient, sensors)
 	}
+}
+
+func main() {
+	fmt.Println("Servidor inicializado.")
+
+	connSensor, err := net.ListenPacket("udp", "localhost:5050")
+	if err != nil {
+		panic(err)
+	}
+	defer connSensor.Close()
+
+	for {
+		sensors := connSensor.ReadFrom()
+	}
+
+	listenerClient, err := net.Listen("tcp", ":5555")
+	if err != nil {
+		panic(err)
+	}
+	defer listenerClient.Close()
+
+	go connectionClient()
+
+	
 }
